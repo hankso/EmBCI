04de455e40015abccbf1386ae11edc7a0c268e74
hank <3080863354@qq.com>
2018-06-09 22:52:23 +0800

    update gpio4 submodule

--- utils/gpio4/constants.py
+++ utils/gpio4/constants.py
@@ -1,29 +1,44 @@
-# edge
-NONE = 'none'
-RISING = 'rising'
-FALLING = 'falling'
-BOTH = 'both'
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 16 18:14:02 2018
 
-# value
-HIGH = 1
-LOW = 0
-
-# direction
-INPUT = 'in'
-OUTPUT = 'out'
-
+@author: hank
+@page:   https://github.com/hankso
+"""
 
 import re
+
 class _sunxi():
     def __getitem__(self, pin):
         rst = re.findall(r"P([A-Z])(\d+)", str(pin))
         if not rst:
-            raise RuntimeError('pin name {} not supported!'.format(pin))
+            raise KeyError('pin name {} not supported!'.format(pin))
         return 32*(ord(rst[0][0])-65) + int(rst[0][1])
 
-# pin mapping
+
+RISING       = 'rising'
+FALLING      = 'falling'
+CHANGE       = 'change'
+HIGH         = 1
+LOW          = 0
+INPUT        = 'in'
+OUTPUT       = 'out'
+INPUT_PULLUP = 'pullup'
+INPUT_PULLDN = 'pulldn'
+MSBFIRST     = 1
+LSBFIRST     = 2
+true         = True
+false        = False
+FOREVER      = 1e5
+FOREVER_ms   = 1e5 * 1000
+
 BOARD_SUNXI = _sunxi()
-
 BOARD_NANO_PI = {}
-
 BOARD_ORANGE_PI_PC = {}
+BCM = {}
+
+__all__ = ['RISING', 'FALLING', 'CHANGE', 'HIGH', 'LOW',
+           'OUTPUT', 'INPUT', 'INPUT_PULLUP', 'INPUT_PULLDN',
+           'MSBFIRST', 'LSBFIRST', 'true', 'false', 'FOREVER', 'FOREVER_ms',
+           'BOARD_SUNXI', 'BOARD_NANO_PI', 'BOARD_ORANGE_PI_PC', 'BCM']
