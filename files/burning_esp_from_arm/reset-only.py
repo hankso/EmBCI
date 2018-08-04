a5adc347d660552ab748614e0d0e1efbfb71604c
hank <3080863354@qq.com>
2018-08-04 20:14:03 +0800

    update esp32 program

--- files/burning_esp_from_arm/reset-only.py
+++ files/burning_esp_from_arm/reset-only.py
@@ -14,9 +14,17 @@ from gpio4 import SysfsGPIO
 
 en = SysfsGPIO(19)
 en.export = True
+
+print('[ESP_Reset] enable value = 0')
 en.direction = 'out'
 en.value = 0
+
 time.sleep(0.2)
+
+print('[ESP_Reset] enable value = 1')
 en.value = 1
 time.sleep(1)
+
+print('[ESP_Reset] soft reset done!')
+
 en.export = False
