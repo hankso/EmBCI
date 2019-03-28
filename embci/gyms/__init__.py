11a0da3ba219fe6f975e4cd53736a84d927446eb
hank <3080863354@qq.com>
2019-03-29 02:25:18 +0800

    sorted output of tools/genrequire.py

--- embci/gyms/__init__.py
+++ embci/gyms/__init__.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+# coding=utf-8
+#
+# File: EmBCI/embci/gyms/__init__.py
+# Author: Hankso
+# Webpage: https://github.com/hankso
+# Time: Fri 29 Mar 2019 02:13:53 CST
+
 from .gym_torcs import TorcsEnv
 from .plane_client import Client as PlaneClient
 
