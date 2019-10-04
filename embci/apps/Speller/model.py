fix embci.drivers.esp32 timestamp float32 precision bug

embci/apps/Speller/model.py
@@ -17,14 +17,13 @@ import time
 import scipy.signal as signal
 import numpy as np
 
-from embci.utils import TempStream
+from embci.utils import TempStream, NameSpace
 with TempStream(stderr=None):
     from keras.utils.generic_utils import Progbar
 
 # =============================================================================
 # constants
 
-NUM_TARGET  = 40   # number of targets (int)
 NUM_SUBBAND = 5    # number of sub-bands (int)
 CHEBY_RP    = 0.4  # maximum ripple allowed in the passband (float)
 CHEBY_GPASS = 3    # maximum loss in the passband (dB)
@@ -35,52 +34,43 @@ CHEBY_FMAX  = 90   # maximum frequency of sub-bands (Hz)
 CHEBY_FEDGE = 10   # edge frequency for maximum frequency of sub-bands (Hz)
 CHEBY_EDGE  = 6    # edge frequency between passband and stopband (Hz)
 
+_default_params = NameSpace(
+    sample_rate=500, num_channel=8, num_sample=0.5*500, num_target=40
+)
+
 
 class Model(object):
-    def __init__(self, reader, ntarget=NUM_TARGET, nsubband=NUM_SUBBAND, **k):
-        self.sample_rate = reader.sample_rate
-        self.num_channel = reader.num_channel
-        self.num_sample  = reader.window_size
-        self.num_subband = nsubband
-        self.num_target  = ntarget
-
-        self.weights = np.zeros((
-            self.num_target, self.num_subband, self.num_channel
+    def __init__(self, sample_rate, num_channel, num_sample,
+                 num_target, num_subband=NUM_SUBBAND, **k):
+        self.sample_rate = sample_rate
+        self.nchannel = num_channel
+        self.nsample  = num_sample
+        self.nsubband = num_subband
+        self.ntarget  = num_target
+        self.weights  = np.zeros((
+            self.ntarget, self.nsubband, self.nchannel
         ))
 
-    @classmethod
-    def from_config(cls, cfg):
-        cfg.setdefault('')
-        return cls(**cfg)
-
-    def update_config(self, cfg):
-        for attr, value in cfg.items():
-            if not attr.startswith('_'):
-                setattr(self, attr, value)
-
-    def get_config(self):
-        pass
-
     @property
-    def num_target(self):
+    def ntarget(self):
         return self._target_num
 
-    @num_target.setter
-    def num_target(self, v):
+    @ntarget.setter
+    def ntarget(self, v):
         if not isinstance(v, int):
             raise TypeError('Expect int but receive: `%s`' % type(v).__name__)
         self._target_num = v
         self._target_coef = np.zeros((
-            self.num_target, self.num_subband,
-            self.num_channel, self.num_sample
+            self.ntarget, self.nsubband,
+            self.nchannel, self.nsample
         ))
 
     @property
-    def num_subband(self):
+    def nsubband(self):
         return self._subband_num
 
-    @num_subband.setter
-    def num_subband(self, v):
+    @nsubband.setter
+    def nsubband(self, v):
         if not isinstance(v, int):
             raise TypeError('Expect int but receive: `%s`' % type(v).__name__)
         elif v < 0 or v > CHEBY_FMAX // CHEBY_FSTEP:
@@ -128,9 +118,9 @@ class Model(object):
 
     def preprocess(self, data):
         nyq = self.sample_rate // 2
-        N, Wn = signal.buttord([7 / nyq, 90 / nyq], [6 / nyq, 92 / nyq], 3, 40)
+        N, Wn = signal.buttord([7 / nyq, 90 / nyq], [5 / nyq, 98 / nyq], 3, 40)
         B, A = signal.butter(N, Wn, 'bandpass')
-        return signal.lfilter(B, A, data)
+        return signal.lfilter(B, A, data, -1)
 
     def plot_subband_freqresp(self):
         try:
@@ -150,70 +140,72 @@ class Model(object):
     def train(self, X):
         '''X should be of shape: n_target x n_channel x n_sample x n_trial'''
         X = np.array(X, ndmin=4)
-        if X.shape[:3] != (self.num_target, self.num_channel, self.num_sample):
+        if X.shape[:3] != (self.ntarget, self.nchannel, self.nsample):
             raise ValueError('Invalid dataset of shape: {}'.format(X.shape))
-        bar = Progbar(target=self.num_target, interval=2)
-        for target in range(self.num_target):
+        bar = Progbar(target=self.ntarget, interval=2)
+        for target in range(self.ntarget):
             # shape: n_channel x n_sample x n_trial
             data = X[target]
             bar.add(0.25)
             time.sleep(0.1)
-            for sb in range(self.num_subband):
-                # shape: n_channel x n_sample x n_trial
-                sbdata = self.subband(data, sb)
-                self._target_coef[target, sb] = np.mean(sbdata, axis=-1)
-                bar.add(0.25 / self.num_subband)
-                time.sleep(0.1)
+            for sb in range(self.nsubband):
                 # shape: n_trial x n_channel x n_sample
-                tsbdata = sbdata.transpose(2, 0, 1)
+                sbdata = self.subband(data.transpose(2, 0, 1), sb)
+                # shape: n_channel x n_sample
+                self._target_coef[(target, sb)] = np.mean(sbdata, axis=0)
+                bar.add(0.25 / self.nsubband)
+                time.sleep(0.01)
                 # calc covariance between trials of data on each channel
                 # Method 1: by dot multiply
-                centered = tsbdata - tsbdata.mean(axis=-1, keepdims=True)
+                centered = sbdata - sbdata.mean(axis=-1, keepdims=True)
+                # shape: [n_trial*n_trial-1] x n_channel x n_channel
                 S = np.sum([
+                    # shape: n_channel x n_channel
                     np.dot(centered[idx1], centered[idx2].T)
-                    for idx1 in range(tsbdata.shape[0] - 1)
-                    for idx2 in range(idx1 + 1, tsbdata.shape[0])
-                ])
-                bar.add(0.25 / self.num_subband)
-                # Method 2: by np.cov
-                #  nch = self.num_channel
+                    for idx1 in range(sbdata.shape[0])
+                    for idx2 in range(sbdata.shape[0])
+                    if idx1 != idx2
+                ], axis=0)
+                bar.add(0.25 / self.nsubband)
+                # Method 2: by covariance
+                #  nch = self.nchannel
                 #  S = np.sum([
                 #      np.cov(tsbdata[idx1], tsbdata[idx2])[:nch, nch:]
-                #      for idx1 in range(tsbdata.shape[0] - 1)
-                #      for idx2 in range(idx1 + 1, tsbdata.shape[0])
+                #      for idx1 in range(sbdata.shape[0])
+                #      for idx2 in range(sbdata.shape[0])
+                #      if idx1 != idx2
                 #  ])
                 # shape: n_channel x (n_sample*n_trial)
-                Q = np.cov(np.concatenate(sbdata, axis=-1))
+                UX = np.concatenate(sbdata, axis=-1)
+                # shape: n_channel x n_channel
+                Q = np.dot(UX, UX.T)
                 value, vector = np.linalg.eig(np.dot(np.linalg.inv(Q), S))
-                self.weights[target, sb] = vector[:, 0]
-                bar.add(0.25 / self.num_subband)
-        bar.update(self.num_target)
-
-    def save(self):
-        pass
+                self.weights[(target, sb)] = vector[:, 0]
+                bar.add(0.25 / self.nsubband)
+        bar.update(self.ntarget)
 
     def predict(self, X, ensemble=False, *a, **k):
         '''X should be of shape: n_target/n_trial x n_channel x n_sample'''
         X = np.atleast_3d(X)
-        assert X.shape[1:] == (self.num_channel, self.num_sample)
+        assert X.shape[1:] == (self.nchannel, self.nsample)
         rst = []
         for idx, trial in enumerate(X):
-            rst.append(self.predict_one_trial(trial, ensemble))
+            rst.append(self.predict_one_trca(trial, ensemble).argmax())
         return np.array(rst)
 
-    def predict_one_trial(self, X, ensemble=False):
+    def predict_one_trca(self, X, ensemble=False):
         '''X should be of shape: n_channel x n_sample'''
         # `rou` holds results of shape: n_target x n_subband
-        rou = np.zeros((self.num_target, self.num_subband))
-        for target in range(self.num_target):
-            for sb in range(self.num_subband):
+        rou = np.zeros((self.ntarget, self.nsubband))
+        for target in range(self.ntarget):
+            for sb in range(self.nsubband):
                 # trained data shape: n_channel x n_sample
                 trained_data = self._target_coef[target, sb]
                 # test data shape: n_channel x n_sample
                 test_data = self.subband(X, sb)
                 if ensemble:
-                    # w should be of shape: n_target x n_channel matrix
-                    w = self.weights[:, sb, :]
+                    # w should be of shape: 1 x n_channel matrix
+                    w = self.weights[:, sb, :].mean(0, keepdims=True)
                 else:
                     # w should be of shape: 1 x n_channel vector
                     w = self.weights[target, sb, :].reshape(1, -1)
@@ -222,15 +214,30 @@ class Model(object):
                 )[0, 1]
         return np.dot(rou, self._subband_coef)  # n_target vector
 
-    def subband_all(self, data):
-        '''data shape: [n_target x] n_channel x n_sample [x n_trial]'''
-        return data
-
     def subband(self, data, sb):
+        '''data shape: [[n_target x] n_trial x] n_channel x n_sample'''
+        assert data.shape[-2:] == (self.nchannel, self.nsample)
         b, a = self._subband_filter[sb]
         return signal.filtfilt(b, a, data, axis=-1, padlen=0)
 
-    def summary(self):
+    def save(self):
         pass
+
+    def load(self):
+        pass
+
+    @classmethod
+    def from_config(cls, cfg):
+        cfg.setdefault('')
+        return cls(**cfg)
+
+    def update_config(self, cfg):
+        for attr, value in cfg.items():
+            if not attr.startswith('_'):
+                setattr(self, attr, value)
+
+    def get_config(self):
+        pass
+
 
 # THE END

embci/drivers/ads1299.py
@@ -100,7 +100,7 @@ SUGGESTED_MSH = np.int32([
 
 @decorator
 def ensure_start(func, self, *a, **k):
-    assert self._started
+    assert self._start_time
     return func(self, *a, **k)
 
 
@@ -180,7 +180,7 @@ class ADS1299_API(spidev.SpiDev):
 
         self._lock = Lock()
         self._opened = False
-        self._started = False
+        self._start_time = 0
         self._enable_bias = False
         self._measure_impedance = False
 
@@ -228,7 +228,7 @@ class ADS1299_API(spidev.SpiDev):
         power supplies have stabilized.
         '''
         assert self._opened, 'you need to open a spi device first'
-        if self._started:
+        if self._start_time != 0:
             return
 
         #  Power up chip
@@ -250,7 +250,7 @@ class ADS1299_API(spidev.SpiDev):
 
         # Start streaming data
         self.write(CMD_RDATAC)
-        self._started = True
+        self._start_time = time.time()
 
     def close(self):
         if not self._opened:
@@ -258,7 +258,7 @@ class ADS1299_API(spidev.SpiDev):
         self.write(CMD_SDATAC)
         self.write(CMD_STOP)
         super(ADS1299_API, self).close()
-        self._started = False
+        self._start_time = 0
         self._epoll.unregister(self._DRDY)
         self._DRDY.export = False
         # self._START.export = False
@@ -361,7 +361,8 @@ class ADS1299_API(spidev.SpiDev):
             tmp = struct.pack('3B', num[3 * i + 2], num[3 * i + 1], num[3 * i])
             # use time: 1.3us
             byte += tmp + ('\xff' if num[3 * i] > 127 else '\x00')
-        return np.frombuffer(byte, np.int32) * self.scale, time.time()
+        data = np.frombuffer(byte, np.int32)
+        return data * self.scale, time.time() - self._start_time
 
     def write(self, byte_array):
         '''Write bytes array to ADS1299 through SPI and return value list.'''

embci/drivers/esp32.py
@@ -55,18 +55,16 @@ class ESP32_API(ADS1299_API):
     embci.drivers.ads1299.ADS1299_API
     '''
     def __init__(self, n_batch=32, scale=4.5/24/2**24, *a, **k):
-        self.n_batch = n_batch
-
         # send `nBatchs * 4Bytes * 8chs` 0x00
         # first 4Bytes is reserved for command to control ESP32
         # [cmd cmd cmd cmd 0x00 0x00 0x00 0x00 ... 0x00]
+        self.n_batch = n_batch
         self._tosend = 4 * 8 * self.n_batch * [0x00]
         self._data_format = '%dB' % len(self._tosend)
         self._cmd_queue = Queue()
         self._data_buffer = []
-        self._last_time = time.time()
-
         super(ESP32_API, self).__init__(scale)
+        self._last_time = time.time()
 
     def open(self, dev, mode=0, max_speed_hz=12.5e6):
         '''ESP32 SPI mode set to 0b00 (CPOL=0 & CPHA=0)'''
@@ -79,17 +77,17 @@ class ESP32_API(ADS1299_API):
         power supplies have stabilized.
         '''
         assert self._opened, 'you need to open a spi device first'
-        if self._started:
+        if self._start_time != 0:
             return
-        self._started = True
         self.set_sample_rate(sample_rate)
+        self._start_time = time.time()
 
     def close(self):
         if not self._opened:
             return
         self._data_buffer = []
         super(ADS1299_API, self).close()
-        self._started = False
+        self._start_time = 0
         self._epoll.unregister(self._DRDY)
         self._DRDY.export = False
         self._opened = False
@@ -100,7 +98,7 @@ class ESP32_API(ADS1299_API):
             cmd = self._cmd_queue.get()
             self.write(cmd + self._tosend[len(cmd):])
             self._data_buffer = []
-            return np.zeros(8, np.float32), self._last_time
+            return np.zeros(8, np.float32), self._last_time - self._start_time
 
         if not len(self._data_buffer):
             # spidev lib is written in C language, where value of list will be
@@ -111,8 +109,8 @@ class ESP32_API(ADS1299_API):
             data = np.frombuffer(data, np.int32).reshape(self.n_batch, 8)
             self._data_buffer = list(data * self.scale)
 
-        starttime = time.time()
-        while (time.time() - starttime) < timeout:
+        ts = time.time()
+        while (time.time() - ts) < timeout:
             dt = time.time() - self._last_time
             if dt < (0.5 / self._sample_rate):
                 time.sleep(0.4 / self._sample_rate)
@@ -124,7 +122,7 @@ class ESP32_API(ADS1299_API):
             else:
                 self._last_time += 1.0 / self._sample_rate
                 break
-        return self._data_buffer.pop(0), self._last_time
+        return self._data_buffer.pop(0), self._last_time - self._start_time
 
     @ensure_start
     def write(self, byte_array):

embci/io/readers.py
@@ -42,7 +42,6 @@ import scipy.io
 import scipy.signal
 import pylsl
 import serial
-from six import string_types
 
 from ..utils import (
     ensure_unicode, ensure_bytes, get_boolean, format_size,
@@ -55,7 +54,7 @@ from ..drivers.esp32 import ESP32_API
 from ..configs import DIR_PID, DIR_TMP
 from . import logger
 
-__all__ = ['valid_name_reader', 'FakeDataGenerator', ] + [
+__all__ = ['validate_readername', 'FakeDataGenerator', ] + [
     _ + 'Reader' for _ in (
         'Files', 'LSL', 'Serial',
         'ADS1299SPI', 'ESP32SPI',
@@ -67,7 +66,7 @@ __all__ = ['valid_name_reader', 'FakeDataGenerator', ] + [
 # Reader MixIn and utilities
 
 _name_reader_pattern = re.compile(r'^(\w+)_(\d+)\.pid$')
-def valid_name_reader(name):                                       # noqa: E302
+def validate_readername(name):                                     # noqa: E302
     '''
     Find suitable name for reader instance in syntax of::
         {ReaderName} {ID}
@@ -211,17 +210,6 @@ class RIOMixin(object):
         data, idx = self._data.copy(), self._index
         return np.concatenate((data[:, idx:], data[:, :idx]), -1)
 
-    def register_buffer(self, obj):
-        if not isinstance(obj, string_types):
-            obj = id(obj)
-        if obj not in self._data_buffer:
-            self._data_buffer[obj] = []
-
-    def unregister_buffer(self, obj):
-        if not isinstance(obj, string_types):
-            obj = id(obj)
-        return np.concatenate(self._data_buffer.pop(obj, [[]]), -1)
-
 
 class RCompatMixin(object):
     '''Methods defined here are for compatibility between all Readers.'''
@@ -299,7 +287,7 @@ class BaseReader(LoopTaskMixin, RIOMixin, RCompatMixin, RStMixin):
         # Broadcast data to a lab-streaming-layer outlet.  Here we only need
         # to check one time whether send_pylsl is True. If put this work in
         # loop task, it will be checked thousands times.
-        self.name = valid_name_reader(name or self.__class__.name)
+        self.name = validate_readername(name or self.__class__.name)
         self._dtype = np.dtype(datatype or self._dtype)
         if get_boolean(send_pylsl):
             if self._dtype.name not in pylsl.pylsl.string2fmt:
@@ -322,7 +310,6 @@ class BaseReader(LoopTaskMixin, RIOMixin, RCompatMixin, RStMixin):
         self._file_data = LockedFile(mmapfn)
         self._data = np.zeros(
             (self.num_channel + 1, self.window_size), self._dtype)
-        self._data_buffer = {}  # TODO: reader._data_buffer name & multiprocess
 
         # Indexs used to output data
         # 0:Channel 1:Frame 2:All 3-5: NotUsed
@@ -428,15 +415,14 @@ class FakeDataGenerator(BaseReader):
 
 
 class FilesReader(BaseReader):
-    '''
-    Read data from mat, fif, csv... file and simulate as a common data reader
-    '''
+    '''Read data from file and simulate as a common data reader.'''
 
     def __init__(self, filename,
                  sample_rate=250, sample_time=2, num_channel=1, **k):
         if not os.path.exist(filename):
             raise ValueError('Data file not exist: `%s`' % filename)
         k.setdefault('input_source', filename)
+        k.setdefault('name', filename + '.Reader')
         super(FilesReader, self).__init__(
             sample_rate, sample_time, num_channel, **k)
 
@@ -547,6 +533,7 @@ class SerialReader(BaseReader):
     Connect to a serial port and fetch data into buffer.
     There should be at least one port available.
     '''
+    name = 'SerialReader'
 
     def __init__(self, sample_rate=250, sample_time=2, num_channel=1, **k):
         super(SerialReader, self).__init__(
@@ -582,6 +569,7 @@ class ADS1299SPIReader(BaseReader):
     '''
     __metaclass__ = Singleton
     API = ADS1299_API
+    name = 'ADS1299Reader'
 
     def __init__(self, sample_rate=250, sample_time=2, num_channel=1,
                  measure_impedance=False, enable_bias=True, API=None, **k):
@@ -665,12 +653,14 @@ class ESP32SPIReader(ADS1299SPIReader):
     This Reader is only used on ARM. It depends on class ESP32_API.
     '''
     API = ESP32_API
+    name = 'ESP32Reader'
 
 
 class SocketTCPReader(BaseReader):
     '''
     A reader that recieve data from specific host and port through TCP socket.
     '''
+    name = 'SocketTCPReader'
 
     def __init__(self, sample_rate=250, sample_time=2, num_channel=1, **k):
         super(SocketTCPReader, self).__init__(
@@ -736,6 +726,7 @@ class SocketTCPReader(BaseReader):
 
 class SocketUDPReader(SocketTCPReader):
     '''Socket UDP client, data receiver. Under development.'''
+    name = 'SocketUDPReader'
 
     def __init__(self, sample_rate=250, sample_time=2, num_channel=1, **k):
         # sample_rate, sample_time, num_channel, name=None